9438e197dfc7c4185e30015348584fb337ae6495
s0h8 <sheetal.singh458@gmail.com>
2021-05-20 10:13:28 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -100,7 +100,7 @@
         },
 
     {
-      question: "Focal length of the lens B is?",  ///// Write the question inside double quotes
+      question: "2. Focal length of the lens B is?",  ///// Write the question inside double quotes
       answers: {
         a: "+120 cm",                  ///// Write the option 1 inside double quotes
         b: "+90 cm",                  ///// Write the option 2 inside double quotes
@@ -110,7 +110,7 @@
       correctAnswer: "c"                ///// Write the correct option inside double quotes
     },
                          {
-      question: "If a concave lens of greater power as compared to the convex lens is kept in the combination such that the distance d =0, then the lens combination will work as:",  ///// Write the question inside double quotes
+      question: "3. If a concave lens of greater power as compared to the convex lens is kept in the combination such that the distance d =0, then the lens combination will work as:",  ///// Write the question inside double quotes
       answers: {
         a: "Convex lens	",                  ///// Write the option 1 inside double quotes
         b: "Plane glass plate",                  ///// Write the option 2 inside double quotes
@@ -120,7 +120,7 @@
       correctAnswer: "c"                ///// Write the correct option inside double quotes
     },
                          {
-      question: "On increasing the distance between the lenses the focal length of the combination:",  ///// Write the question inside double quotes
+      question: "4. On increasing the distance between the lenses the focal length of the combination:",  ///// Write the question inside double quotes
       answers: {
         a: "decreases",                  ///// Write the option 1 inside double quotes
         b: "increases",                  ///// Write the option 2 inside double quotes
